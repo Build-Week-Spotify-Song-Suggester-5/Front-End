782466875a0a07b3f01ae50c236f50379e49421b
nick migel <nickmigel2@gmail.com>
2020-03-05 18:28:40 -0500

    styled abit

--- spotify-song-suggester/src/components/SuggestedSongs.js
+++ spotify-song-suggester/src/components/SuggestedSongs.js
@@ -2,22 +2,22 @@ import React from "react";
 import '../App.css'
 
 export const SuggestedSongs = props => {
-console.log(props);
-const songList = props.suggestedSongs.map((song, i) => {
+  console.log(props);
+  const songList = props.suggestedSongs.map((song, i) => {
     return (
-        <div className='jams' key={i}>
-            <h2>{song.track_name}</h2>
-            <p>{song.artist_name}</p>
-        </div>
+      <div className='rec' key={i}>
+        <h2>{song.track_name}</h2>
+        <p>{song.artist_name}</p>
+      </div>
     )
-    });
+  });
 
   return (
     <div className="suggestions">
-        <h1>Suggested Songs</h1>
-        <div>
-        {songList}  
-        </div>
+      <h1>Suggested Songs</h1>
+      <div>
+        {songList}
+      </div>
     </div>
   );
 };
